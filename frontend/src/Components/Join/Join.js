7de9ad81cb29274fc68721c470aaee1be77e1ea6
Sahil Rajput <sahilrajput85579@gmail.com>
2021-01-28 23:37:16 +0530

    Updated Front Page Component

--- frontend/src/Components/Join/Join.js
+++ frontend/src/Components/Join/Join.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Join.css";
 
 const Join = () => {
-    return <div>Join</div>
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join Chat Room</h1>
+        <div>
+          <input
+            name="name"
+            className="joinInput"
+            placeholder="Name"
+            type="text"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            name="room"
+            className="joinInput mt-20"
+            placeholder="Room Name"
+            type="text"
+            onChange={(event) => setRoom(event.target.value)}
+          />
+        </div>
+        <Link
+          onClick={(event) => (!name || !room ? event.preventDeafult() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className="button mt-20" type="submit">
+            Enter Room
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Join;
