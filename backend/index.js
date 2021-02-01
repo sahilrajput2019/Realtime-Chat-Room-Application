034f200e3cbdd7ff2226dc1d50a24ef7fb803157
Sahil Rajput <sahilrajput85579@gmail.com>
2021-02-01 22:32:36 +0530

    Project Completed

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,78 @@
 const express = require("express");
-const socketio = require("socket.io");
 const http = require("http");
+const socketio = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);               // Instance of socket.io
-
-const router  = require("./Router/router.js");
-
-app.use(router);    //Middleware
+app.use(cors());
+const io = socketio(server, {
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+});
 
 const PORT = process.env.PORT || 5000;
 
+const router = require("./Router/router.js");
+const {
+  addUser,
+  getUser,
+  removeUser,
+  getUsersInRoom,
+} = require("./Utils/helper");
+const { use } = require("./Router/router.js");
 
-server.listen(PORT, () => console.log(`Server is Running ${PORT}`) );
+app.use(router); //Middleware
+;
+
+//specific client instance
+io.on("connection", (socket) => {
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit("message", {
+      user: "Admin",
+      text: `${user.name}, welcome to the ${user.room}.`,
+    }); 
+
+    socket.broadcast.to(user.room).emit("message", {
+      user: "Admin",
+      text: `${user.name} has joined the room.`,
+    });
+
+    io.to(user.room).emit("roomData", {      //For updating the status of Users in Room
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+    callback();  //ususally for error handling from client side to trigger this
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "Admin",
+        text: `${user.name} has left the chat room.`,
+      });
+
+      io.to(user.room).emit("roomData", {      //For updating the status of Users in Room
+        room: user.romm,
+        users: getUsersInRoom(user.room),
+      });
+    }
+  });
+});
+
+server.listen(PORT, () => console.log(`Server is Running ${PORT}`));
--- frontend/src/Components/Chat/Chat.js
+++ frontend/src/Components/Chat/Chat.js
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-const Chat = () => {
-    return <div>Chat</div>
+import TextContainer from "../TextContainer/TextContainer";
+import Messages from "../Messages/Messages";
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+
+import "./Chat.css";
+
+let socket;
+const ENDPOINT = "localhost:5000";
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit("join", { name, room }, (error) => {
+      if (error) {
+        alert(error);
+      }
+    });
+  }, [ENDPOINT, location.search]);
+
+  //for handling Messages
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+  }, [messages]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+      <TextContainer users={users} />
+    </div>
+  );
 };
 
 export default Chat;
